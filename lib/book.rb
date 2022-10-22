2b638f749c6a9e2ffe3c5919339cde9276772ea3
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-10-22 14:30:55 +0300

    complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :genre , :page_count
 
+    def initialize(title)
+        @title = title
+        @author = "Agatha Christie"
+        @page_count = 272
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+novel = Book.new("Ugly Love")
+novel.title
